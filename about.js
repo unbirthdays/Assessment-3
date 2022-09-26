121427ef5603130991d7a3999a1dc5413c97ad3d
Sabrina Baccam <sabrinabaccam@gmail.com>
2022-09-26 18:28:24 -0500

    save

--- about.js
+++ about.js
@@ -5,10 +5,17 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert(('Submitted successfully!'));
+}
+
+function catAndMouse(evt) {
+	evt.preventDefault();
+	alert(("You're a cat person? You must be really cool!"));
 }
 
 
 let form = document.querySelector('#contact');
+let cat = document.querySelector('#cat');
 
 form.addEventListener('submit', handleSubmit);
+cat.addEventListener('mouseover', catAndMouse);
